fd7dd5d74d1f315030f43d5051bf26c9892a2efb
arschedev <98528463+arschedev@users.noreply.github.com>
2023-07-08 01:02:43 +0300

    Basic movement system

--- src/TetrisGame.java
+++ src/TetrisGame.java
@@ -11,7 +11,7 @@ public class TetrisGame extends JFrame {
     }
 
     private void init() {
-        setTitle("Tetris");
+        setTitle("Tetris (p to start)");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
         Board board = new Board();
@@ -34,23 +34,30 @@ class Board extends JPanel implements KeyListener {
     private final int TILE_SIZE = 40;
     private final int BOARD_WIDTH = 10;
     private final int BOARD_HEIGHT = 20;
-    private final Color DEFAULT_TILE_COLOR = Color.BLACK;
     private final Color DEFAULT_BORDER_COLOR = Color.WHITE;
+    private final Color DEFAULT_TILE_COLOR = Color.BLACK;
+    private final Color ACTIVE_TILE_COLOR = Color.RED;
     private final Color[][] boardGrid; // 2d array of colors
+    private int xPos = Utils.random(0, 9);
+    private int yPos = -1;
 
     public Board() {
         setPreferredSize(new Dimension(TILE_SIZE * BOARD_WIDTH, TILE_SIZE * BOARD_HEIGHT));
         boardGrid = new Color[BOARD_HEIGHT][BOARD_WIDTH]; // initialize board
         fillBoard();
-
-         fillTile(0, 0, Color.RED);
-         fillTile(BOARD_WIDTH - 1, 0, Color.RED);
-         fillTile(BOARD_WIDTH - 1, BOARD_HEIGHT - 1, Color.RED);
-         fillTile(0, BOARD_HEIGHT - 1, Color.RED);
     }
 
-    private void fillTile(int x, int y, Color color) {
-        boardGrid[y][x] = color;
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for (int row = 0; row < BOARD_HEIGHT; row++) {
+            for (int col = 0; col < BOARD_WIDTH; col++) {
+                g.setColor(boardGrid[row][col]);
+                g.fillRect(col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                g.setColor(DEFAULT_BORDER_COLOR);
+                g.drawRect(col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+            }
+        }
     }
 
     private void fillBoard() {
@@ -63,65 +70,73 @@ class Board extends JPanel implements KeyListener {
                 boardGrid[row][col] = color;
             }
         }
+
     }
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        render(g);
-    }
-
-    private void render(Graphics g) {
-        for (int row = 0; row < BOARD_HEIGHT; row++) {
-            for (int col = 0; col < BOARD_WIDTH; col++) {
-                g.setColor(boardGrid[row][col]);
-                g.fillRect(col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-                g.setColor(DEFAULT_BORDER_COLOR);
-                g.drawRect(col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-            }
-        }
-    }
-
-    private void movement() {
+    private void init() {
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
+                // floor limit
+                if (yPos + 1 >= BOARD_HEIGHT) {
+                    // new active tile
+                    xPos = Utils.random(0, 9);
+                    yPos = -1;
+                }
+
+                yPos++; // increment y position
+                if (yPos > 0) fillTile(xPos, yPos - 1, DEFAULT_TILE_COLOR); // remove previous tile
+                fillTile(xPos, yPos, ACTIVE_TILE_COLOR); // fill current tile
+                repaint(); // Redrawing the board
             }
         }, 0, 1000);
     }
 
-    private int random(int min, int max) {
-        return (int) Math.floor(Math.random() * (max - min + 1) + min);
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-
+    private void fillTile(int x, int y, Color color) {
+        boardGrid[y][x] = color;
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if (e.getKeyChar() == 'a') {
-            fillTile(4, 4, Color.WHITE);
-            fillTile(3,4,Color.white);
-            fillTile(5,4,Color.white);
-            fillTile(4,3,Color.white);
-            fillTile(4,2,Color.white);
-            repaint(); // Redrawing the board
+        if (e.getKeyChar() == 'p') {
+            init();
         }
 
+        if (e.getKeyChar() == 'a') {
+            fillTile(xPos, yPos, DEFAULT_TILE_COLOR); // remove previous tile
+            if (xPos > 0) xPos--; // decrement x position
+            fillTile(xPos, yPos, ACTIVE_TILE_COLOR); // fill current tile
+        }
+
+        if (e.getKeyChar() == 'd') {
+            fillTile(xPos, yPos, DEFAULT_TILE_COLOR); // remove previous tile
+            if (xPos < BOARD_WIDTH - 1) xPos++; // increment x position
+            fillTile(xPos, yPos, ACTIVE_TILE_COLOR); // fill current tile
+        }
+
+        if (e.getKeyChar() == 's') {
+            fillTile(xPos, yPos, DEFAULT_TILE_COLOR); // remove previous tile
+            if (yPos < BOARD_HEIGHT - 1) yPos++; // increment y position
+            fillTile(xPos, yPos, ACTIVE_TILE_COLOR); // fill current tile
+        }
+
+        repaint(); // Redrawing the board
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        if (e.getKeyChar() == 'a') {
-            fillTile(4,4,Color.BLACK);
-            fillTile(3,4,Color.black);
-            fillTile(5,4,Color.black);
-            fillTile(4,3,Color.black);
-            fillTile(4,2,Color.black);
-            repaint();
-        }
+        // TODO
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO
+    }
+}
+
+class Utils {
+    public static int random(int min, int max) {
+        return (int) Math.floor(Math.random() * (max - min + 1) + min);
     }
 }
