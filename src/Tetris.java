b24764f80b2cb23f3c152a2c6fb28e2b82f4339c
arschedev <98528463+arschedev@users.noreply.github.com>
2023-07-10 00:30:18 +0300

    Update Tetris.java
    clear filled lines feature, unstable, undone

--- src/Tetris.java
+++ src/Tetris.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Arrays;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -33,15 +34,13 @@ class Board extends JPanel implements KeyListener {
     private final int TILE_SIZE = 30;
     private final int BOARD_WIDTH = 10;
     private final int BOARD_HEIGHT = 20;
-    private final Color[][] boardGrid; // 2d array of colors
+    private Color[][] boardGrid; // 2d array of colors
     private final Color DEFAULT_BORDER_COLOR = Color.WHITE;
     private final Color DEFAULT_TILE_COLOR = Color.BLACK;
     private Color ACTIVE_TILE_COLOR = Utils.randomColor();
-    private char currentTetromino = Utils.randomTetromino();
+    private char currentTetromino = 'O';
     private int xPos = Utils.random(1, 7);
     private int yPos = -1;
-    private int linesCleared = 0;
-
 
     public Board() {
         setPreferredSize(new Dimension(TILE_SIZE * BOARD_WIDTH, TILE_SIZE * BOARD_HEIGHT));
@@ -52,6 +51,7 @@ class Board extends JPanel implements KeyListener {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+
         for (int row = 0; row < BOARD_HEIGHT; row++) {
             for (int col = 0; col < BOARD_WIDTH; col++) {
                 g.setColor(boardGrid[row][col]);
@@ -60,20 +60,6 @@ class Board extends JPanel implements KeyListener {
                 g.drawRect(col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE);
             }
         }
-        for (int row = 0; row < BOARD_HEIGHT; row++) {
-            boolean lineFilled = true;
-            for (int col = 0; col < BOARD_WIDTH; col++) {
-                if (boardGrid[row][col] == DEFAULT_TILE_COLOR) {
-                    lineFilled = false;
-                    break;
-                }
-            }
-            if (lineFilled) {
-                g.setColor(Color.GRAY);
-                g.fillRect(0, row * TILE_SIZE, BOARD_WIDTH * TILE_SIZE, TILE_SIZE);
-            }
-        }
-
     }
 
     private void fillBoard() {
@@ -96,9 +82,12 @@ class Board extends JPanel implements KeyListener {
             public void run() {
                 // reached limits of floor or other tetrominos
                 if (yPos + 1 >= BOARD_HEIGHT || tetrominoWillCollide(xPos, yPos, currentTetromino, "down")) {
+                    // clear filled lines
+                    clearLines();
+
                     // new active tetromino
                     ACTIVE_TILE_COLOR = Utils.randomColor();
-                    currentTetromino = Utils.randomTetromino();
+                    currentTetromino = 'O';
                     xPos = Utils.random(1, 7);
                     yPos = -1;
                 }
@@ -108,32 +97,34 @@ class Board extends JPanel implements KeyListener {
                     fillTetromino(xPos, yPos - 1, DEFAULT_TILE_COLOR, currentTetromino); // remove previous tetromino
                 fillTetromino(xPos, yPos, ACTIVE_TILE_COLOR, currentTetromino); // fill current tetromino
                 repaint(); // Redrawing the board
-                clearLines();
             }
         }, 0, 1000);
     }
-    public int getLinesCleared() {
-        return linesCleared;
-    }
 
     private void clearLines() {
-        for (int row = BOARD_HEIGHT - 1; row >= 0; row--) {
-            boolean lineFilled = true;
+        for (int row = BOARD_HEIGHT - 1; row > 0; row--) {
+            boolean isLineFilled = true;
+
             for (int col = 0; col < BOARD_WIDTH; col++) {
                 if (boardGrid[row][col] == DEFAULT_TILE_COLOR) {
-                    lineFilled = false;
+                    isLineFilled = false;
                     break;
                 }
             }
-            if (lineFilled) {
-                for (int r = row; r > 0; r--) {
-                    System.arraycopy(boardGrid[r - 1], 0, boardGrid[r], 0, BOARD_WIDTH);
-                }
-                for (int col = 0; col < BOARD_WIDTH; col++) {
-                    boardGrid[0][col] = DEFAULT_TILE_COLOR;
-                }
-                linesCleared++;
+
+            if (isLineFilled) {
+                // FIXME
+                boardGrid = Utils.removeLastElementFromColor2D(boardGrid);
+                boardGrid = Utils.prependArrayToColor2D(boardGrid, new Color[BOARD_WIDTH]);
+                fillLine(0, DEFAULT_TILE_COLOR);
+                row++;
             }
+        }
+    }
+
+    private void fillLine(int row, Color color) {
+        for (int col = 0; col < BOARD_WIDTH; col++) {
+            fillTile(col, row, color);
         }
     }
 
@@ -273,24 +264,28 @@ class Board extends JPanel implements KeyListener {
         if (e.getKeyChar() == 'p' || e.getKeyChar() == 'з') {
             init();
         }
+
         if (e.getKeyChar() == 'a' || e.getKeyChar() == 'ф') {
             fillTetromino(xPos, yPos, DEFAULT_TILE_COLOR, currentTetromino); // remove previous tetromino
             if (!tetrominoWillCollide(xPos, yPos, currentTetromino, "left")) // is aside from wall and other tetrominos
                 xPos--; // decrement x position
             fillTetromino(xPos, yPos, ACTIVE_TILE_COLOR, currentTetromino); // fill current tetromino
         }
+
         if (e.getKeyChar() == 'd' || e.getKeyChar() == 'в') {
             fillTetromino(xPos, yPos, DEFAULT_TILE_COLOR, currentTetromino); // remove previous tetromino
             if (!tetrominoWillCollide(xPos, yPos, currentTetromino, "right")) //  is aside from wall and other tetrominos
                 xPos++; // increment x position
             fillTetromino(xPos, yPos, ACTIVE_TILE_COLOR, currentTetromino); // fill current tetromino
         }
+
         if (e.getKeyChar() == 's' || e.getKeyChar() == 'ы' || e.getKeyChar() == 'і') {
             fillTetromino(xPos, yPos, DEFAULT_TILE_COLOR, currentTetromino); // remove previous tetromino
             if (yPos < BOARD_HEIGHT - 1 && !tetrominoWillCollide(xPos, yPos, currentTetromino, "down")) // is above floor level and other tetrominos
                 yPos++; // increment y position
             fillTetromino(xPos, yPos, ACTIVE_TILE_COLOR, currentTetromino); // fill current tetromino
         }
+
         repaint(); // Redrawing the board
     }
 
@@ -318,5 +313,21 @@ class Utils {
     public static Color randomColor() {
         final Color[] colors = {Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW, Color.CYAN, Color.MAGENTA, Color.ORANGE, Color.PINK};
         return colors[random(0, colors.length - 1)];
+    }
+
+    public static Color[][] removeLastElementFromColor2D(Color[][] originalArray) {
+        return Arrays.copyOf(originalArray, originalArray.length - 1);
+    }
+
+    public static Color[][] prependArrayToColor2D(Color[][] originalArray, Color[] newArray) {
+        int rowsNum = originalArray.length;
+        int colsNum = originalArray[0].length;
+        int newRowsNum = originalArray.length + 1;
+
+        Color[][] resultArray = new Color[newRowsNum][colsNum];
+        resultArray[0] = Arrays.copyOf(newArray, colsNum);
+        System.arraycopy(originalArray, 0, resultArray, 1, rowsNum);
+
+        return resultArray;
     }
 }
